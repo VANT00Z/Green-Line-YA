31ce97f3c14283e3abea3b928adf0443837af072
huh <ivangalaktionov24@gmail.com>
2026-03-05 13:05:36 +0300

    mini html and py update

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ def delivery():
     return render_template('html/delivery.html')
 
 
-@app.route('/info')
-def info():
+@app.route('/ex-info')
+def ex_info():
     return render_template('html/ex_info.html')
 
 
@@ -26,5 +26,10 @@ def contacts():
     return render_template('html/contacts.html')
 
 
+@app.route('/info')
+def info():
+    return render_template('html/information.html')
+
+
 if __name__ == "__main__":
-    app.run(port=8000)
+    app.run(port=8000, debug=True)
