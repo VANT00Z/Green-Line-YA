fe376dd16dc3b7770f4785b704d4ffedf4b36ce1
huh <ivangalaktionov24@gmail.com>
2026-03-23 15:15:03 +0300

    Medium update (backend)
    Update db and joing,
    Update html (little),
    form works

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect
 from back.data.models.users_model import UserModel
 from back.data.models.order_model import OrderModel
 from back.forms.user_form import RegisterForm, LoginForm
@@ -7,6 +7,7 @@ from back.data import db_session
 
 # DB
 from flask_login import LoginManager
+from flask_login import login_user, login_required, logout_user, current_user
 
 
 app = Flask(__name__)
@@ -21,9 +22,56 @@ def load_user(user_id):
     return session.get(UserModel, user_id)
 
 
+@app_manager.user_loader
+def load_user(user_id):
+    session = db_session.create_session()
+    return session.get(UserModel)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        session = db_session.create_session()
+        user = session.query(UserModel).filter(
+            UserModel.email == form.mail.data).first()
+        if user and user.check_password(form.password.data):
+            return redirect("/delivery")
+        return redirect("/")    # Переадресовка: неправильный пароль или логин
+    return redirect("/")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.ex_password.data:
+            return redirect('/index')   # Переадресовка: повтор пароля неверный
+        session = db_session.create_session()
+        if session.query(UserModel).filter(UserModel.email == form.mail.data).first():
+            return ("/index")   # Переадресовка: Пользователь уже есть
+        user = UserModel(
+            name=form.name.data,
+            mail=form.mail.data
+        )
+        user.set_password(form.password.data)
+        session.add(user)
+        session.commit()
+        return redirect('/index')
+    return redirect('/index')
+
+
 @app.route('/')
 @app.route('/index')
 def index():
+    session = db_session.create_session()
     return render_template('html/index.html')
 
 
@@ -45,17 +93,6 @@ def contacts():
 @app.route('/info')
 def info():
     return render_template('html/information.html')
-
-
-@app.route('/register', methods=['POST'])
-def register():
-    form = RegisterForm()
-    print('anus')
-
-
-@app.route('/auth',  methods=['POST'])
-def auth():
-    form = LoginForm()
 
 
 def main():
