0e9b346460293e9f7be0fa7621a92a6c952ec2bb
huh <ivangalaktionov24@gmail.com>
2026-05-12 16:29:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -294,7 +294,7 @@ def info():
 
 def main():
     db_session.global_init('db.sqlite')
-    app.run(port=8000, debug=True)
+    app.run(port=80, host='0.0.0.0.', debug=True)
 
 
 if __name__ == "__main__":
